56c3b5dd390642a97f976f7b589769aa2ad4feb0
diego-nz <nunez.guzman@gmail.com>
2017-03-19 11:12:43 -0600

    Puse header y footer en otro archivo y solo los mande a llamar en el index.

--- index.php
+++ index.php
@@ -6,46 +6,16 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="assets/css/font-awesome.min.css">
     <link rel="stylesheet" href="assets/css/estilosPrincipales.css">
-    <title>Inicio ropa</title>
+    <title>Gifts</title>
 </head>
 
 <body>
     <header>
-        <div class="menu">
-            <ul>
-                <li><a href="#"><i class="fa fa-home fa-fw"></i>Inicio</a></li>
-                <li><a href="#"><i class="fa fa-user fa-fw"></i>Mi cuenta</a></li>
-                <li><a href="#"><i class="fa fa-heart fa-fw"></i>Favoritos</a></li>
-                <li><a href="#"><i class="fa fa-lock fa-fw"></i>Inicio sesión</a></li>
-                <li><a href="#"><i class="fa fa-building fa-fw"></i>Empresa</a></li>
-                <li><a href="#"><i class="fa fa-info fa-fw"></i>Contacto</a></li>
-            </ul>
-        </div>
+        <?php require_once("view/estaticas/header.html"); ?>
     </header>
-    <div class="capaLogo">
-        <div class="logo">
-            <img src="assets/logo.png" alt="GIFTS">
-        </div>
-        <div class="buscador">
-            <input type="text" placeholder="buscador" id="txtBuscar">
-        </div>
-        <div class="carrito">
-            <ul>
-                <li><i class="fa fa-shopping-cart fa-2x" style="color:#00BED7;"></i> Mi carrito</li>
-                <li style="color:#00BED7;">(0) elementos | $0.00</li>
-            </ul>
-        </div>
-    </div>
-    <nav>
-        <ul>
-            <li><a href="#">Hogar</a></li>
-            <li><a href="#">Joyeria y relojes</a></li>
-            <li><a href="#">Aniversarios</a></li>
-            <li><a href="#">Más vendidos</a></li>
-            <li><a href="#">Cumpleaños</a></li>
-        </ul>
-    </nav>
     <section>
+
+        <!----Aqui va el slider--->
         <div class="slider">
 
         </div>
@@ -63,7 +33,13 @@
         </div>
 
         <div class="productos">
-            <?php for ($i=1 ; $i <=2 ; $i++) { $prodAleatorio=rand(1,55); $prodAleatorio2=rand(1,55); $prodAleatorio3=rand(1,55); $prodAleatorio4=rand(1,55); $prodAleatorio5=rand(1,55); ?>
+            <?php for ($i=1 ; $i <=2 ; $i++) {
+                $prodAleatorio=rand(1,55);
+                $prodAleatorio2=rand(1,55);
+                $prodAleatorio3=rand(1,55);
+                $prodAleatorio4=rand(1,55);
+                $prodAleatorio5=rand(1,55);
+            ?>
             <div class="imagenProducto">
                 <a href="productos.php?pro="><img src="imagenes/productos/<?php echo $prodAleatorio; ?>.png" alt="" /></a>
                 <div class="precioProducto">
@@ -129,24 +105,7 @@
         </div>
         <footer>
 
-            <div id="redesSociales">
-               <ul>
-                   <li><i class="fa fa-facebook-square fa-2x" style="color:#02527F;"></i> Facebook</li>
-                
-                   <li><i class="fa fa-twitter-square fa-2x" style="color: #006D95;"></i> Twitter</li>
-                
-                   <li><i class="fa fa-youtube-square fa-2x" style="color:#8C1825;"></i> Youtube</li>
-               </ul>
-                
-            </div>
-            <div id="informacion">
-                <ul>
-                    <li>Información sobre la tienda</li>
-                    <li>Pc Store, Tecámac, Cp. 55749, Tecámac Estado de México</li>
-                    <li>Llámanos ahora: 5588503194</li>
-                    <li>Email: contacto@pcstore.com</li>
-                </ul>
-            </div>
+            <?php require_once("view/estaticas/footer.html"); ?>
         </footer>
     </section>
 
